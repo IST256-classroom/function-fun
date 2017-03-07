98408ae1ec72b059c3b6d174113a732907aa932b
Nick <ndlyga@syr.edu>
2017-03-06 19:09:34 -0500

    code fixes

--- function-fun.solution.py
+++ function-fun.solution.py
@@ -1,8 +1,7 @@
 '''
-We are going to write an advanced game of guess the word to demonstrate how we use funtions
+We are going to write an advanced game of guess the work to demonstrate how we use funtions
 Below are 7 functions that are used for the main program. Each Group needs to define their function
-with the right inputs and it needs to return the proper value. The function name is given along with what inputs
-it needs and what it should return.
+with the right inputs and it needs to return the proper value. The function name is given
 '''
 
 # Import statements are always at the top
@@ -48,13 +47,6 @@ them to see if they are equal. Return True if the are, False if they aren't
 '''
 # Group 2 Function Here
 
-def check_guessed_word(x,y):
-    if x==y:
-        print('true')
-    else:
-        print('false')
-    return check_guessed_word
-
 
 '''
 Group 3:
@@ -62,17 +54,6 @@ Write a function named check_character_in_word, It should take 2 str and return 
 str_1 is in str_2, or return false if its not
 '''
 # Group 3 Function Here
-def check_character_in_word(str_1, str_2):
-    for c in str_2:
-        if c == str_1:
-            return 'True'
-        elif c != str_1:
-            return 'False'
-
-str_1 = input("Enter a character: ")
-str_2 = input("Enter some text: ")
-y = check_character_in_word(str_1, str_2)
-print(y)
 
 
 '''
@@ -82,13 +63,6 @@ occurances of str_1 in str_2 and replace them by index in str_3 and return the s
 There are a couple of ways to do this
 '''
 # Group 4 Function Here
-def replace_letters_in_word(str1,str3):
-    str1 = str1.split(str3)
-    return str1
-str1 = 'abcdefghijk'
-str3 = 'e'
-output = replace_letters_in_word(str1,str3)
-print(output)
 
 
 '''
@@ -98,12 +72,6 @@ a string with the same amount of _ as the original string: Ex: take example retu
 '''
 # Group 5 Function Here
 
-def generate_blanks(string1):
-    num_blanks = '_' * len(string1)
-    return num_blanks
-
-user_input = input("Enter text: ")
-print(generate_blanks(user_input))
 
 '''
 Group 6:
@@ -112,9 +80,6 @@ and return the new string.
 '''
 # Group 6 Function Here
 
-def save_guess(str_1, str_2):
-    new_str = (str_2 + str_1)
-    return(new_str)
 
 
 '''
@@ -139,19 +104,23 @@ def main():
         guess = input("Please guess the word, or guess a letter: ")
         trys = trys + 1
         if len(guess) > 1:
-            # Group 7 work here!
+            if check_guessed_word(guessed_word, random_word):
+                break
+            else:
+                print("Nice Try, but that's not the word!")
         else:
-            # Group 7 Work here!
+            if check_character_in_word(guess, random_word):
+                print("Good Job, you got one!")
+                guessed_word = replace_letters_in_word(guess, random_word, guessed_word)
+            else:
+                print("Wrong! Choose again")
+            letters_guessed = save_guess(letters_guessed, guess)
             if not "_" in guessed_word:
                 break
     
     print("Nice Work!")
-    # Print the result like "You guessed the word: 'example' in 10 trys" using format!
-    # Group 7 do this too!
+    print("You guessed the word: {} in {} trys".format(random_word, trys))
 
-
-# Call the main function to start the program
-main()
 
 
 
